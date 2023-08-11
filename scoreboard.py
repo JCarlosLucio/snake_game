de2d786ba1957198c04417de29d5b76e3a6fb63e
Juan Carlos Lucio <c.lucio3@gmail.com>
2023-08-10 19:22:56 -0600

    Add highscore to Scoreboard

--- scoreboard.py
+++ scoreboard.py
@@ -8,6 +8,7 @@ class Scoreboard(Turtle):
     def __init__(self) -> None:
         super().__init__()
         self.score = 0
+        self.high_score = 0
         self.hideturtle()
         self.color("white")
         self.penup()
@@ -16,12 +17,18 @@ class Scoreboard(Turtle):
 
     def increase_score(self) -> None:
         self.score += 1
-        self.clear()
         self.update_scoreboard()
 
     def update_scoreboard(self) -> None:
-        self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
+        self.clear()
+        self.write(
+            f"Score: {self.score} Highscore: {self.high_score}",
+            align=ALIGNMENT,
+            font=FONT,
+        )
 
-    def game_over(self):
-        self.goto(0, 0)
-        self.write("GAME OVER", align=ALIGNMENT, font=FONT)
+    def reset(self) -> None:
+        if self.score > self.high_score:
+            self.high_score = self.score
+        self.score = 0
+        self.update_scoreboard()
